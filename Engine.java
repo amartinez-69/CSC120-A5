b07e887abc796624e9a24b1f731fc85f83b1c498
Your Nameamartinez69@smith.edu <you@example.com>
2023-03-06 21:28:39 -0500

    Engine file done

--- Engine.java
+++ Engine.java
@@ -1,3 +1,42 @@
 public class Engine {
+    
+    private FuelType typeFuel; //type of fuel used
+    double maxFuel; //maxium fuel (100.0)
+    double currentFuel; //engine's current fuel level
 
+    public Engine(FuelType typeFuel, double currentFuel){ //sets engine mechanics 
+        this.typeFuel = typeFuel; 
+        this.currentFuel = currentFuel;
+
+    }
+
+    public void refuel(){ //sets currentFuel to 100.0 
+        if(this.currentFuel < 100.0){
+            this.currentFuel = 100.0; 
+        }
+    }
+
+    public void go(){ //decreases currentFuel level by 10.0, prints out fuel level after every decrease
+        this.currentFuel = currentFuel -= 10.0; 
+        if(this.currentFuel >= 0){
+            System.out.println("The current fuel level is " + this.currentFuel);
+        }
+        else{
+            throw new RuntimeException("The train engine is out of fuel. Please refuel."); //When fuel is 0.0, throws exception
+        }
+    }
+
+    public static void main(String[] args) {
+        Engine myEngine = new Engine(FuelType.ELECTRIC, 100.0);
+        try {
+            while (true) {
+                myEngine.go();
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage()); // Out of fuel
+        }
+        myEngine.refuel(); 
+        System.out.println(myEngine.currentFuel);
+
+    }
 }
